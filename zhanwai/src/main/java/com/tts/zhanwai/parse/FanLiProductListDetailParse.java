752783d4a4e6bc0e35aad75c36588f714d712b1e
hulk <hulk@localhost>
2016-07-22 20:10:58 +0800

    fanli

--- zhanwai/src/main/java/com/tts/zhanwai/parse/FanLiProductListDetailParse.java
+++ zhanwai/src/main/java/com/tts/zhanwai/parse/FanLiProductListDetailParse.java
@@ -7,14 +7,35 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 
+import com.tts.zhanwai.downloader.ProductListDownloader;
+import com.tts.zhanwai.downloader.SimpleDownloader;
+import com.tts.zhanwai.model.DownloadType;
+import com.tts.zhanwai.model.Method;
 import com.tts.zhanwai.model.ProductListDetail;
+import com.tts.zhanwai.model.UrlType;
+import com.tts.zhanwai.service.ZhanWaiProductService;
+import com.tts.zhanwai.utils.Constants;
+import com.tts.zhanwai.utils.LogUtils;
 
 @Cacheable
 public class FanLiProductListDetailParse extends ProductListParse {
 	private String url = "http://9.fanli.com/?cid=3&p=18";
 	private List<ProductListDetail> productListDetails = new ArrayList<ProductListDetail>();
+	private static final String pageQuery = "&page=";
+	private Map<String, String> header;
+	private static final Logger loger = LogUtils.getLogger(FanLiProductListDetailParse.class);
+	@Autowired
+	private ProductListDownloader productListDownloader;
+	@Autowired
+	private SimpleDownloader simpleDownloader;
+	@Autowired
+	private SimpleParseTaobaoId simpleParseTaobaoId;
+	@Autowired
+	private ZhanWaiProductService zhanWaiProductService;
 
 	@Override
 	public List<ProductListDetail> parseHtml(String html) {
@@ -56,13 +77,37 @@ public class FanLiProductListDetailParse extends ProductListParse {
 		return pageProductListDetails;
 	}
 
+	public boolean hasDataShow(String html) {
+		Pattern htmlPattern = Pattern.compile("<div class=\"nine-item[\\s\\S]*?(<div class=\"amount\">)");
+		Matcher htmlMatcher = htmlPattern.matcher(html);
+		return htmlMatcher.find();
+	}
+
 	@Override
 	public List<ProductListDetail> parseHtmlBody(String html) {
 		// TODO Auto-generated method stub
 		List<ProductListDetail> tmpProductListDetail = new ArrayList<ProductListDetail>();
 		tmpProductListDetail = parseHtml(html);
+		int pageCount = 2;
+		while (hasDataShow(html)) {
+			DownloadType downloadType = new DownloadType();
+			downloadType.setCookie(header.get(Constants.COOKIE));
+			downloadType.setMethod(Method.GET);
+			downloadType.setUrl(getCategory().getCategoryUrl() + pageQuery + pageCount);
+			downloadType.setUrlType(UrlType.PRODUCTLIST);
+			downloadType.setUser_agent(header.get(Constants.USER_AGETNT));
+			downloadType.setReferer(header.get(Constants.REFERE));
+			// logger.error(downloadType.getUrl());
 
-		return super.parseHtmlBody(html);
+			CloseableHttpResponse httpResponse = productListDownloader.startDownload(downloadType);
+			html = parseResponse(httpResponse, header);
+			List<ProductListDetail> tmProductListDetails = parseHtml(html);
+			productListDetails.addAll(tmProductListDetails);
+			tmProductListDetails.clear();
+			tmProductListDetails = null;
+			pageCount++;
+		}
+		return productListDetails;
 	}
 
 	@Override
